792733eb1a71ce05d826a3ebabe0082940905ba4
salammustafa728 <salammustafa728@gmail.com>
2022-03-27 20:44:16 +0200

    edit auth

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ console.log('working');
 
 const userIn = io.of('/user');
 
+
+// if you want to use token and auth with socket to get user info using that token
+userIn.use((socket,next)=>{
+    if (socket.handshake.auth.token) {
+        socket.username = getUsernameFromToken(socket.handshake.auth.token)
+        next()
+    }
+    else {
+        next(new Error("please send Token"))
+    }
+})
+function getUsernameFromToken(token){
+return token
+}
+userIn.on('connection',socket=>{
+    console.log('connected to namespace user');
+})
+
 //run every time the client connect to server
 io.on('connection',socket=>{
     console.log(socket.id);
@@ -27,21 +45,5 @@ io.on('connection',socket=>{
     })
     socket.on('ping',n=>console.log(n))
 })
+
 instrument(io,{auth:false})
-
-
-// if you want to use token and auth with socket to get user info using that token
-// userIn.use((socket,next)=>{
-//    if(socket.handshacke.auth.token){
-//        socket.username = getUsernameFromToken(socket.handshacke.auth.token);
-//     next();
-//     }else{
-//         next(new Error('please send token'));
-//     }
-// })
-// function getUsernameFromToken(token){
-// return token
-// }
-userIn.on('connection',socket=>{
-    console.log('connected to namespace user');
-})
